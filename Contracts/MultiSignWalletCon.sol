331b28e0bd9bc2d344d813f1f5fa154eb0a50cc7
Conduent <wasim.bari@conduent.com>
2020-10-15 21:47:40 +0530

    removed unused reference

--- Contracts/MultiSignWalletCon.sol
+++ Contracts/MultiSignWalletCon.sol
@@ -3,8 +3,6 @@
 
 pragma solidity ^0.5.0; // ^0.6.6;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 
 /*
 
