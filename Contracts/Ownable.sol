9b51f58ce6b8a9caec0a00c6c9abf9388d9047fe
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2020-10-22 00:17:58 +0530

    Create Ownable.sol

---
+++ Contracts/Ownable.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract Ownable {
+
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+}
