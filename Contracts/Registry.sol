8554991181d93366153ea2cd582594279f00af23
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2020-10-22 00:17:34 +0530

    Create Registry.sol

---
+++ Contracts/Registry.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.0;
+
+import './Ownable.sol';
+import './Storage.sol';
+
+contract Registry is Storage, Ownable {
+
+    address public logic_contract;
+    //address public ownerAddr;
+  
+    // admin to set contract
+    function setLogicContract(address _c) public onlyOwner returns (bool success){
+        logic_contract = _c;
+        return true;
+    } 
+    
+    constructor() public {
+   
+    }
+    
+
+
+    // fall back function
+    function () payable external {
+
+        address target = logic_contract;
+
+        assembly {
+            // Copy the data sent to the memory address starting free mem position
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+
+            // Proxy the call to the contract address with the provided gas and data
+            let result := delegatecall(gas, target, ptr, calldatasize, 0, 0)
+
+            // Copy the data returned by the proxied call to memory
+            let size := returndatasize
+            returndatacopy(ptr, 0, size)
+
+            // Check what the result is, return and revert accordingly
+            switch result
+            case 0 { revert(ptr, size) }
+            case 1 { return(ptr, size) }
+        }
+    }
+
+}
